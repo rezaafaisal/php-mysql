8e09e66305135d6d46a77bbabf2bc7ce9737fe1c
rezaafaisal <rezha.faizal567@gmail.com>
2023-01-24 17:32:40 +0800

    fontawesome

--- partials/asset_footer.php
+++ partials/asset_footer.php
@@ -4,3 +4,4 @@
   <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/e8b696d2f5.js" crossorigin="anonymous"></script>
